78764c9fc2c547664b818d3e7f3dd116c30208b2
Haoran Feng <harrifeng@gmail.com>
2018-07-20 10:24:18 +0800

    go on

--- src/components/TasksPage.js
+++ src/components/TasksPage.js
@@ -4,6 +4,46 @@ import TaskList from './TaskList';
 const TASK_STATUSES = ['Unstarted', 'In Progress', 'Completed'];
 
 class TasksPage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showNewCardForm: false,
+      title: '',
+      description: '',
+    };
+  }
+
+  onTitleChange = (e) => {
+    this.setState({ title: e.target.value });
+  }
+
+  onDescriptionChange = (e) => {
+    this.setState({ description: e.target.value });
+  }
+
+  resetForm() {
+    this.setState({
+      showNewCardForm: false,
+      title: '',
+      description: '',
+    });
+  }
+
+  onCreateTask = (e) => {
+    e.preventDefault();
+    this.props.onCreateTask({
+      title: this.state.title,
+      description: this.state.description,
+    });
+    this.resetForm();
+  }
+
+  toogleForm = () => {
+    this.setState({ showNewCardForm: !this.state.showNewCardForm });
+  }
+
+
   renderTasksLists() {
     const { tasks } = this.props;
 
@@ -15,9 +55,42 @@ class TasksPage extends Component {
 
   render() {
     return (
-      <div className="tasks">
-        <div className="task-lists">
+      <div className="task-list">
+        <div className="task-list-header">
+          <button className="button button-default"
+                  onClick={this.toogleForm}
+                  >
+            + New task
+          </button>
+        </div>
+        {this.state.showNewCardForm && (
+          <form className="task-list-form" onSubmit={this.onCreateTask}>
+            <input
+              className="full-width-input"
+              onChange={this.onTitleChange}
+              value={this.state.title}
+              type="text"
+              placeholder="title"
+              />
+            <input
+              className="full-width-input"
+              onChange={this.onDescriptionChange}
+              value={this.state.description}
+              type="text"
+              placeholder="description"
+              />
+            <button
+              className="button"
+              type="submit"
+              >
+              Save
+            </button>
+          </form>
+        )}
+        <div className="tasks">
+          <div className="task-lists">
           {this.renderTasksLists()}
+          </div>
         </div>
       </div>
     );
